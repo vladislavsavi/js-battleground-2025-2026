1da17ef36caaba1aef51ee8f10f229b98d9f3c8a
vladislavsavin <v.savin@mycar.digital>
2025-11-17 11:36:48 +0500

    Refactor sum functions for improved readability and performance

--- dealing_with_recursion/sum.js
+++ dealing_with_recursion/sum.js
@@ -11,4 +11,25 @@ function sum(arr) {
     return arr[0] + sum(arr.slice(1, arr.length));
 }
 
-console.log(sum([1, 2, 3, 4, 5])); // 15
+
+function sum_1(arr, i) {
+    if(i === arr.length - 1) {
+        return arr[i];
+    }
+
+    return arr[i] + sum_1(arr, i + 1);
+}
+
+function sum_2(arr) {
+    const helper = (i) => {
+        if(i === arr.length - 1) {
+            return arr[i];
+        }
+
+        return arr[i] + helper(i + 1);
+    }
+
+    return helper(0);
+}
+
+console.log(sum_2([1, 2, 3, 4, 5], )); // 15
