2ae03685e2d09239b2b86ff467660fb134d2dcf2
vladislavsavin <v.savin@mycar.digital>
2025-11-17 17:46:19 +0500

    Refactor permutations function for clarity and update console log to use permutations_1

--- dealing_with_recursion/616.js
+++ dealing_with_recursion/616.js
@@ -12,13 +12,32 @@ function permutations(str) {
 
     for (let i = 0; i < str.length; i++) {
         const variants = permutations(str.replace(str[i], ''))
-        const variantsWithChr = variants.map((str1) => str[i] + str1);
+        const variantsWithChr = variants.map((variant) => str[i] + variant);
         results.push(...variantsWithChr);
     };
 
     return results;
 }
 
+function permutations_1(str) {
+    if (str.length === 1) { 
+        return str[0];
+    }
 
-console.log(permutations("ABC"));
+    const results = [];
+    let substringsAnagrams = permutations_1(str.slice(1));
+    console.log(substringsAnagrams);
+    
+    for (let i = 0; i < substringsAnagrams.length; i++) {
+        for (let j = 0; j <= substringsAnagrams[i].length; j++) {
+            const newAnagram = substringsAnagrams[i].slice(0, j) + str[0] + substringsAnagrams[i].slice(j);
+            results.push(newAnagram);
+        }
+    }
+
+    return results;
+}
+
+
+console.log(permutations_1("ABC"));
 // ["ABC", "ACB", "BAC", "BCA", "CAB", "CBA"]
