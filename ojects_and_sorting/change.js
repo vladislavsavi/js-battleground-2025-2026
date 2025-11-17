b776bb85c55e9cee05a3e4c41ece22f7b3700df8
vladislavsavin <v.savin@mycar.digital>
2025-11-17 10:13:39 +0500

    Add Coin Change algorithm and Bubble Sort implementation

---
+++ ojects_and_sorting/change.js
@@ -0,0 +1,28 @@
+//  Coin Change Problem
+// Функция принимает массив монет и сумму, возвращает минимальное количество монет для этой суммы или -1, если сумма недостижима.
+
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+const coinChange = (coins, amount) => {
+    const dp = new Array(amount + 1).fill(Infinity);
+    dp[0] = 0;
+
+    for (let coin of coins) {
+        for (let x = coin; x <= amount; x++) {
+            dp[x] = Math.min(dp[x], dp[x - coin] + 1);
+        }
+    }
+    
+    const result = dp[amount];
+    if (result === Infinity) {
+        console.log(-1);
+        return -1;
+    } else {
+        return result;
+    }
+};
+
+console.log(coinChange([1, 3, 4], 6));
