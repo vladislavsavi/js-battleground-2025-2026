505d18087995c4024fbf5ef68b65cfd5c749f21b
vladislavsavin <v.savin@mycar.digital>
2025-11-18 09:41:04 +0500

    Add toEvenList function to filter even numbers from an array

---
+++ dealing_with_recursion/evenFilter.js
@@ -0,0 +1,17 @@
+
+function toEvenList(arr, i = 0) {
+    if (arr.length === i) {
+        return [];
+    }
+
+    const nextEvens = toEvenList(arr, i + 1);
+
+    if (arr[i] % 2 === 0) {
+        return [arr[i], ...nextEvens];
+    } else {
+        return [...nextEvens];
+    }
+}
+
+
+console.log(toEvenList([1, 2, 3, 4, 5, 6, 7, 8])); // [2,4,6,8]
