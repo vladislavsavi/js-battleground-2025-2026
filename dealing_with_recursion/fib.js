74dcaefae5b338f76f780875dc6e4c0e56bae397
vladislavsavin <v.savin@mycar.digital>
2025-11-19 16:34:11 +0500

    Add Fibonacci function with memoization for efficient computation

---
+++ dealing_with_recursion/fib.js
@@ -0,0 +1,15 @@
+
+
+function fib(n, memo = {}) {
+    if (n === 0 || n === 1) {
+        return n;
+    }
+
+    if (!memo[n]) {
+        memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+    }
+
+    return memo[n];     
+}
+
+console.log(fib(1000));
