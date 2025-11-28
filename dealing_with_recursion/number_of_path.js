7da26a3f9b7442ea724a75e2c2a9cda716fdf32f
vladislavsavin <v.savin@mycar.digital>
2025-11-28 16:52:46 +0500

    Refactor numberOfPaths function to use memoization for improved performance

--- dealing_with_recursion/number_of_path.js
+++ dealing_with_recursion/number_of_path.js
@@ -2,10 +2,11 @@
 //  Функция принимает число n и возвращает количество способов добраться до вершины,
 //  если можно подниматься на 1, 2 или 3 шага за раз.
 
-function numberOfPaths(n) {
+function numberOfPaths(n, memo = {}) {
     if (n < 0) return 0;
     if (n === 0 || n === 1) return 1;
-    return numberOfPaths(n -1) + numberOfPaths(n - 2) + numberOfPaths(n - 3);
+    memo[n] = memo[n] || numberOfPaths(n - 1, memo) + numberOfPaths(n - 2, memo) + numberOfPaths(n - 3, memo);
+    return memo[n] ;
 }
 
-console.log(numberOfPaths(3)); // 4
+console.log(numberOfPaths(10)); // 4
